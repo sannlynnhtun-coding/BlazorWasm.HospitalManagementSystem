6c1f06f4e8ad0ff0488fa62837922d354b386782
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-28 04:16:22 +0630

    [Complete] Doctor Specialist Role CRUD

---
+++ BlazorWasm.HospitalManagementSystem/Models/DoctorSpecialistRoleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWasm.HospitalManagementSystem.Models;
+
+public record DoctorSpecialistRoleModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
--- BlazorWasm.HospitalManagementSystem/Services/ApiUrl.cs
+++ BlazorWasm.HospitalManagementSystem/Services/ApiUrl.cs
@@ -4,4 +4,8 @@ public static class ApiUrl
 {
     public static string Diseases { get; } = "Diseases";
     public static string Rooms { get; } = "Rooms";
+    public static string DoctorSpecialistRole { get; } = "doctor-specialist";
+    public static string DoctorSpecialistRoleCreate { get; } = "doctor-specialist/create";
+    public static string DoctorSpecialistRoleEdit { get; } = "doctor-specialist/edit";
+    public static string DoctorSpecialistRoleDelete { get; } = "doctor-specialist/delete";
 }
