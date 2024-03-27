cef783e421e2b64ee0a761c661f231ae2b12e9e9
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-28 00:35:48 +0630

    [Complete] MainLayout

--- BlazorWasm.HospitalManagementSystem/Program.cs
+++ BlazorWasm.HospitalManagementSystem/Program.cs
@@ -1,10 +1,13 @@
 using BlazorWasm.HospitalManagementSystem;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
