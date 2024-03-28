61a8c8def0fb7561f75c1cb258d58e5f42f173eb
Zin Linn Htet <mackk5504@gmail.com>
2024-03-28 14:05:24 +0630

    [Add] Global Using

---
+++ BlazorWasm.HospitalManagementSystem/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using BlazorWasm.HospitalManagementSystem;
+global using BlazorWasm.HospitalManagementSystem.Models;
+global using BlazorWasm.HospitalManagementSystem.Services;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.FluentUI.AspNetCore.Components;
