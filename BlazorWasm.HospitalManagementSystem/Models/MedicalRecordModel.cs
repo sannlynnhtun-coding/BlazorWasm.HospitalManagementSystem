eda7b97f5cf877993e299851187f0c20d7e3c6f6
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-31 02:26:36 +0630

    [Complete] Medical Record

---
+++ BlazorWasm.HospitalManagementSystem/Models/MedicalRecordModel.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorWasm.HospitalManagementSystem.Models;
+
+public class MedicalRecordModel
+{
+    public int Id { get; set; }
+    public int PatientID { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Diagnosis { get; set; }
+    public string Note { get; set; }
+    public string Treatment { get; set; }
+    public List<DiseaseModel> Diseases { get; set; }
+}
+
+public class MedicalRecordViewModel
+{
+    public int Id { get; set; }
+    public int PatientId { get; set; }
+    public string PatientName { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Diagnosis { get; set; }
+    public string Note { get; set; }
+    public string Treatment { get; set; }
+    public List<DiseaseModel> Diseases { get; set; }
+}
